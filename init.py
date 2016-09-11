b506d26ed047b4a5ca16303ac68c24576171916d
josuecb <josue.carbonel@live.lagcc.cuny.edu>
2016-09-10 22:58:01 -0400

    Deleting useless comments
    Deleting useless comments

--- init.py
+++ init.py
@@ -4,5 +4,3 @@ if __name__ == '__main__':
     rar = RarCrackFile('crackMeTest.rar', 10, 'dictionary.txt')
     # rar.create_threads()
     rar.start()
-    # dic = DictionaryGen(4)
-    # dic.write_file()
