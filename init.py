3fad9bb5a595f63f5049ca700d10dadfb737754e
josuecb <josue.carbonel@live.lagcc.cuny.edu>
2016-09-10 22:57:31 -0400

    Deleted useless imports
    Deleting useless imports

--- init.py
+++ init.py
@@ -1,6 +1,4 @@
-from TimerDelay import *
 from RarCrackFile import *
-from DictionaryGen import DictionaryGen
 
 if __name__ == '__main__':
     rar = RarCrackFile('crackMeTest.rar', 10, 'dictionary.txt')
